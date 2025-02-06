a7880140539c5b4e2048c13c508312adc5685f48
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2025-02-06 16:19:43 +0700

    added unit test

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,11 @@ repositories {
     mavenCentral()
 }
 
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -31,13 +36,11 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation ("org. seleniumhq. selenium: selenium-java: $seleniumJavaVersion")
-    testImplementation("io.github.bonigarcia: selenium-jupiter : $seleniumJupiterVersion")
-    testImplementation ("io. github. bonigarcia: webdrivermanager: $webdrivermanagerVersion")
-    testImplementation ("org. junit. jupiter : junit-jupiter-api: $junitJupiterVersion")
-    testRuntimeOnly("org.junit. jupiter: junit-jupiter-engine : $junitJupiterVersion")
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.14.1")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:5.0.1")
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.6.3")
 }
+
 
 tasks.register<Test>( "unitTest") {
     description = "Runs unit tests."
@@ -61,8 +64,3 @@ tasks.register<Test>( "unitTest") {
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
-
-val seleniumJavaVersion = "4.14.1"
-val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
