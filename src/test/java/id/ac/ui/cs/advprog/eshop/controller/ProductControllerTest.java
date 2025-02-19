49343acae08e9b1903252fe9ab53070bbdef9008
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2025-02-19 13:53:12 +0700

    code quality fix Remove this unused import 'org.springframework.ui.Model'.

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -12,7 +12,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
